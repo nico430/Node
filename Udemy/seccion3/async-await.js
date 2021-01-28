042fce2febc8057f2890769894a5d389eb90ece1
nicolas <nico.chabanpavon@gmail.com>
2021-01-28 00:23:10 -0300

    async

--- Udemy/seccion3/async-await.js
+++ Udemy/seccion3/async-await.js
@@ -2,25 +2,42 @@
 Async Await
 */
 
-let getNombre = async() => {
+/*let getNombre = async() => {
 
     throw new Error('No existe un nombre para el usuario');
 
     return 'Nicolas';
 
-};
+};*/
 //esto es lo mismo que hacer:
-/*
+
 let getNombre = () => {
-    return new Pormise( (resolve, response) => {
-        resolve ('Fernando')
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve('Nicolas');
+            reject('No existe un nombre para el usuario')
+        }, 3000);
+
     });
 }
-*/
 
-getNombre().then(nombre => {
-        console.log(nombre);
+
+//el encadenamiento de promesas con async await es mas sencillo ya que 
+//poniendo await a la promeas que se espera parecera un trabajo sincrono 
+//ya que esperaria a la priomesa necesaria antes de ejecutar la actual.
+
+let saludo = async() => {
+    let nombre = await getNombre();
+
+    return `Hola ${nombre}`
+}
+
+
+
+saludo().then(mensaje => {
+        console.log(mensaje);
     })
-    .catch(e => {
+    /*.catch(e => {
         console.log('Error de ASYNC', e);
-    })
+    })*/
