46009bb52bbfe38d13334ee9544228ac59889adc
nicolas <nico.chabanpavon@gmail.com>
2021-01-28 15:06:48 -0300

    process y argumentos

--- Udemy/03-BASES-NODE/app.js
+++ Udemy/03-BASES-NODE/app.js
@@ -1,6 +1,13 @@
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 'a';
+//console.log(process.argv) process viene en node y muestra una cantidad inmensa de datos utiles
+//el argv muestra unicamente los argumentos que halla ingresados en consola por defecto se mustra donde se encuentra node y donde se encuentra el archivo ejecutado
+
+
+let argv = process.argv;
+let parametro = argv[2]; //tercer parametro ingresado
+let base = parametro.split('=')[1]; //separa la frase por el simbolo indicado y selecciono el 2 elemento
+
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
